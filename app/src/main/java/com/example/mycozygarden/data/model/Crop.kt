c708bdd8056a4dafc22bf75f3e84b643f840f741
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-06 23:33:39 +0700

    Update Crop.kt

--- app/src/main/java/com/example/mycozygarden/data/model/Crop.kt
+++ app/src/main/java/com/example/mycozygarden/data/model/Crop.kt
@@ -6,20 +6,20 @@ enum class CropType {
 
 data class Crop(
     val type: CropType,
-    val nameRes: Int,
+    val name: String,
     val priceToPlant: Int,
     val baseHarvestValue: Int,
     val baseGrowthTimeSec: Int
 ) {
     companion object {
         val all = listOf(
-            Crop(CropType.POTATO, R.string.potato, 10, 30, 100),
-            Crop(CropType.TOMATO, R.string.tomato, 20, 60, 120),
-            Crop(CropType.SUNFLOWER, R.string.sunflower, 50, 150, 180),
-            Crop(CropType.STRAWBERRY, R.string.strawberry, 100, 300, 240),
-            Crop(CropType.PUMPKIN, R.string.pumpkin, 300, 900, 360),
-            Crop(CropType.CORN, R.string.corn, 80, 240, 200),
-            Crop(CropType.PEPPER, R.string.pepper, 120, 360, 220)
+            Crop(CropType.POTATO, "Картофель", 10, 30, 100),
+            Crop(CropType.TOMATO, "Помидор", 20, 60, 120),
+            Crop(CropType.SUNFLOWER, "Подсолнух", 50, 150, 180),
+            Crop(CropType.STRAWBERRY, "Клубника", 100, 300, 240),
+            Crop(CropType.PUMPKIN, "Тыква", 300, 900, 360),
+            Crop(CropType.CORN, "Кукуруза", 80, 240, 200),
+            Crop(CropType.PEPPER, "Перец", 120, 360, 220)
         )
         fun getByType(type: CropType) = all.find { it.type == type }!!
     }
